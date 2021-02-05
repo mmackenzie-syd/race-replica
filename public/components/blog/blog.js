73761d27363a37c723a91dafa659ae69a024d860
mmackenzie-syd <mmackenzie.syd@gmail.com>
2021-02-05 23:24:52 +1100

    add blog page

---
+++ public/components/blog/blog.js
@@ -0,0 +1,13 @@
+
+class Blog extends View {
+    constructor() {
+        super();
+        this.template = 'components/blog/blog.html';
+    }
+
+    view() {
+        return {};
+    }
+}
+
+const blog = new Blog();
