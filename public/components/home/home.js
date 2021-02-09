8580d53f338081e3bb6edf45cda7bafea5ecccd0
mmackenzie-syd <mmackenzie.syd@gmail.com>
2021-02-09 11:40:53 +1100

    correct home page initial state

--- public/components/home/home.js
+++ public/components/home/home.js
@@ -1,55 +1,58 @@
 
+
 class Home extends View {
   constructor() {
     super();
     this.template = 'components/home/home.html';
+    this.state = {
+        slide: 17,
+        bikeColor: 'RED'
+    }
+  }
+  view() {
+      // const image = `slide/SV800MY20-${bikeColor}${slide}.png`;
+      // return {
+      //     image
+      // }
+      // this method causes flicker, so commented until I work out why
   }
 
-  view() {
-    return {};
+  setImage() {
+      // temporary until work out the flickering
+      const { bikeColor, slide } = this.state;
+      const img = document.getElementById('img');
+      const src = `slide/SV800MY20-${bikeColor}${slide}.png`
+      img.src = src;
+  }
+
+  onBikeColorClick(bikeColor) {
+    this.setState({ bikeColor: bikeColor });
+    console.log('state', this.state, bikeColor)
+    // app.render(this);
+      this.setImage();
+  }
+
+  // onInput() {} for slider that as removed for the moment
+
+  nextSlide() {
+    let { slide } = this.state;
+    if ((slide - 1) > -1) {
+        slide = slide -1;
+        this.setState({ slide })
+        // app.render(this); -- causes flicker
+        this.setImage();
+    }
+  }
+
+  prevSlide() {
+    let { slide } = this.state;
+    if ((slide + 1) < 36) {
+        slide = slide + 1;
+        this.setState({ slide })
+        // app.render(this); -- causes flicker
+        this.setImage()
+    }
   }
 }
 
 const home = new Home();
-
-let bikeColor = 'RED'; // default color
-
-let slide = 17;
-
-function setSlide() {
-    // const slide = document.getElementById("slider").value;
-    // const img = document.getElementById('img');
-    // const src = `slide/SV800MY20-${bikeColor}${slide}.png`
-    // img.src = src;
-    const img = document.getElementById('img');
-    const src = `slide/SV800MY20-${bikeColor}${slide}.png`
-    img.src = src;
-}
-
-function onBikeColorClick(color) {
-    bikeColor = color;
-    setSlide();
-}
-
-function onInput() {
-    setSlide();
-}
-
-function nextSlide() {
-    console.log('clicked')
-    if ((slide - 1) > -1) {
-        slide = slide -1;
-        const img = document.getElementById('img');
-        const src = `slide/SV800MY20-${bikeColor}${slide}.png`
-        img.src = src;
-    }
-}
-
-function prevSlide() {
-    if ((slide + 1) < 36) {
-        slide = slide + 1;
-        const img = document.getElementById('img');
-        const src = `slide/SV800MY20-${bikeColor}${slide}.png`
-        img.src = src;
-    }
-}
